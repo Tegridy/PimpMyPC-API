e0dcad0f22c03f405a8d4e86ee672045d6a589d9
Tegridy <jarjak4@gmail.com>
2022-04-15 18:25:41 +0200

    Fix saving order bug

--- src/main/java/com/pimpmypc/api/order/OrderServiceImpl.java
+++ src/main/java/com/pimpmypc/api/order/OrderServiceImpl.java
@@ -2,8 +2,10 @@ package com.pimpmypc.api.order;
 
 import com.pimpmypc.api.auth.AuthService;
 import com.pimpmypc.api.cart.CartService;
+import com.pimpmypc.api.exception.AuthenticationException;
 import com.pimpmypc.api.exception.OrderNotFoundException;
 import com.pimpmypc.api.exception.UserNotFoundException;
+import com.pimpmypc.api.order.dto.CustomerPersonalDataDto;
 import com.pimpmypc.api.order.dto.OrderDto;
 import com.pimpmypc.api.order.dto.OrderResponse;
 import com.pimpmypc.api.product.Product;
@@ -37,21 +39,26 @@ public class OrderServiceImpl implements OrderService {
 
 
     @Override
-    public OrderResponse saveOrder(Order order) {
+    public OrderResponse saveOrder(CustomerPersonalDataDto customerData) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        order.getProducts().forEach(product -> {
+        cartService.getCustomerProductsInCart().forEach(product -> {
             product.setQuantity(product.getQuantity() - 1);
             product.setNumberOfItemsSold(product.getNumberOfItemsSold() + 1);
         });
 
-        order.getDeliveryAddress().setCreatedAt(LocalDateTime.now());
+        Order order = new Order();
+        order.setCustomerFirstName(customerData.getCustomerFirstName());
+        order.setCustomerLastName(customerData.getCustomerLastName());
         order.setOrderStatus(OrderStatus.IN_PROGRESS);
+        order.setCustomerEmail(customerData.getCustomerEmail());
+        order.setCustomerPhone(customerData.getCustomerPhone());
+        order.setTotalPrice(cartService.calculateCartTotalPrice());
+        order.setProducts(cartService.getCustomerProductsInCart());
+
+        customerData.getDeliveryAddress().setCreatedAt(LocalDateTime.now());
         order.setCreatedAt(LocalDateTime.now());
         order.setModifiedAt(LocalDateTime.now());
-        order.setProducts(cartService.getCustomerProductsInCart());
-        order.setTotalPrice(cartService.calculateCartTotalPrice());
-
 
         if (authentication.isAuthenticated()) {
             userRepository.findByUsername(authentication.getName()).ifPresent((user) -> {
@@ -60,9 +67,8 @@ public class OrderServiceImpl implements OrderService {
             });
         }
 
-
-        log.info("Saving order: " + order.getId());
         Order savedOrder = orderRepository.save(order);
+        log.info("Saving order: " + savedOrder.getId());
         cartService.clearCart();
 
         return OrderResponse.builder().id(savedOrder.getId()).status(savedOrder.getOrderStatus()).build();
@@ -82,20 +88,28 @@ public class OrderServiceImpl implements OrderService {
             userOrders = user.getUserOrders().stream().map(order -> OrderResponse.builder().status(order.getOrderStatus())
                     .id(order.getId()).price(order.getTotalPrice()).orderDate(order.getCreatedAt().toLocalDate())
                     .build()).toList();
+        } else {
+            log.warn("User is not logged in. Can't load orders");
         }
 
         return new PageImpl<OrderResponse>(userOrders, pageable, 9);
     }
 
     @Override
-    public OrderDto getUserOrderDetails(Long id) {
-        Order order = orderRepository.findById(id)
-                .orElseThrow(() -> new OrderNotFoundException("Order with id: " + id + " not found."));
-        List<Product> orderProducts = order.getProducts();
+    public OrderDto getUserOrderDetails(Long id) throws AuthenticationException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            Order order = orderRepository.findById(id)
+                    .orElseThrow(() -> new OrderNotFoundException("Order with id: " + id + " not found."));
+            List<Product> orderProducts = order.getProducts();
 
-        return OrderDto.builder().id(order.getId()).title("Order: " + order.getId())
-                .imageUrl(orderProducts.get(0).getImageUrl()).products(orderProducts).address(order.getDeliveryAddress())
-                .price(orderProducts.stream().map(Product::getPrice).reduce(new BigDecimal(0), BigDecimal::add))
-                .build();
+            return OrderDto.builder().id(order.getId()).title("Order: " + order.getId())
+                    .imageUrl(orderProducts.get(0).getImageUrl()).products(orderProducts).address(order.getDeliveryAddress())
+                    .price(orderProducts.stream().map(Product::getPrice).reduce(new BigDecimal(0), BigDecimal::add))
+                    .build();
+        } else {
+            log.warn("User don't have access to see this order");
+            throw new AuthenticationException("User don't have access to see this order");
+        }
     }
 }
---
+++ src/main/java/com/pimpmypc/api/order/dto/CustomerPersonalDataDto.java
@@ -0,0 +1,24 @@
+package com.pimpmypc.api.order.dto;
+
+import com.pimpmypc.api.user.address.Address;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@ToString
+public class CustomerPersonalDataDto {
+    @Size(min = 3, max = 55)
+    private String customerFirstName;
+    @Size(min = 3, max = 55)
+    private String customerLastName;
+    @Size(min = 9, max = 12)
+    private String customerPhone;
+    @Email
+    private String customerEmail;
+    private Address deliveryAddress;
+}
